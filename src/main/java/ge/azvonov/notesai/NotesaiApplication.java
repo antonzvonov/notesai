dda21fefb174bab2eb3afcce674b836eca4122cd
Anton <>
2025-06-06 12:38:30 +0300

    Исправил зависимости

--- src/main/java/ge/azvonov/notesai/NotesaiApplication.java
+++ src/main/java/ge/azvonov/notesai/NotesaiApplication.java
@@ -2,9 +2,12 @@ package ge.azvonov.notesai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class NotesaiApplication {
+public class NotesaiApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(NotesaiApplication.class, args);
