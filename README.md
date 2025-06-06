8eececabec73ab613bda81321e2dc2f0293e966c
Anton Zvonov <97289279+antonzvonov@users.noreply.github.com>
2025-06-06 12:00:12 +0300

    Rename base package to ge.azvonov

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# notesai
+# Notesai
+
+Простое веб-приложение на Spring Boot с приветственной страницей.
+
+## Запуск
+
+```
+mvn spring-boot:run
+```
+
+После запуска откройте `http://localhost:8080` и увидите сообщение «Hello, World!». 
---
+++ src/test/java/ge/azvonov/notesai/NotesaiApplicationTests.java
@@ -0,0 +1,12 @@
+package ge.azvonov.notesai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
