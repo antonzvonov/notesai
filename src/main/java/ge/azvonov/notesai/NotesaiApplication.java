ceb8fb97d3bbbcbe2099c4c9b7eff47b05437d9f
Anton Zvonov <97289279+antonzvonov@users.noreply.github.com>
2025-06-06 12:03:46 +0300

    Merge pull request #1
    Рыба

---
+++ src/main/java/ge/azvonov/notesai/NotesaiApplication.java
@@ -0,0 +1,12 @@
+package ge.azvonov.notesai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesaiApplication.class, args);
+    }
+}
